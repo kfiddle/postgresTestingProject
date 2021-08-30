35bbab1a0f1a5a23c408421032ffa35d4a5de6fd
kfiddle <kenjfiddle@gmail.com>
2021-08-30 11:48:31 -0400

    first commit

---
+++ src/main/java/com/example/demo/models/Person.java
@@ -0,0 +1,37 @@
+package com.example.demo.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/com/example/demo/repositories/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
